914e8a0769fcd7fa7e2526133768f4abfc49a537
SunilKumar12345-creator <sunil.kumar@amplicomm.com>
2025-05-31 22:13:28 +0530

    testeninghello1

--- sample 2/src/main/java/com/samplePro/sample/SampleApplication.java
+++ sample 2/src/main/java/com/samplePro/sample/SampleApplication.java
@@ -9,6 +9,7 @@ public class SampleApplication {
   public static void main(String[] args) {
     SpringApplication.run(SampleApplication.class, args);
     System.out.println("Hello..!");
+    System.out.println("Hello1..!");
   }
 
 }
